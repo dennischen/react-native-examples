a2316cc9d9c4051a69c149ac5e98bd9def77e13f
Dennis Chen <atticcat@gmail.com>
2023-12-27 15:27:04 +0800

    fix "bundler": "metro" doesn't fast refresh

--- appEntry.js
+++ appEntry.js
@@ -1,3 +1,8 @@
+
+//https://github.com/expo/expo/issues/23104
+//import @expo/metro-runtime to fix fast refres in web when using bundler metro
+import "@expo/metro-runtime";
+
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 import App from './src/apps/App';
