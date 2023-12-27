a96ba6e004fa474aa9320713bb6388e584ebeb2e
Dennis Chen <atticcat@gmail.com>
2023-12-27 15:15:53 +0800

    add assets

--- src/screens/Scroll.tsx
+++ src/screens/Scroll.tsx
@@ -3,6 +3,8 @@ import { appStyles } from '@/appStyles'
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 
+const favicon  = require('@assets/favicon.png')
+
 const logo = {
     uri: 'https://reactnative.dev/img/tiny_logo.png',
     width: 64,
@@ -19,41 +21,41 @@ const styles = StyleSheet.create({
 })
 
 export default function Scroll() {
-
+    
     let i = 0
     return (
         <View style={appStyles.screen}>
             <ScrollView style={styles.scroll}>
                 <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+                <Image source={favicon} />
                 <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
+                <Image source={favicon} />
                 <Text style={{ fontSize: 96 }}>If you like</Text>
                 <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
                 <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+                <Image source={favicon} />
                 <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
+                <Image source={favicon} />
                 <Text style={{ fontSize: 96 }}>What's the best</Text>
                 <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
                 <Text style={{ fontSize: 96 }}>Framework around?</Text>
+                <Image source={favicon} />
                 <Image source={logo} />
+                <Image source={favicon} />
                 <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
-                <Image source={logo} />
+                <Image source={favicon} />
                 <Text style={{ fontSize: 80 }}>React Native</Text>
             </ScrollView>
         </View>
