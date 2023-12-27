9d4b151022dd0e79d4ac06dbff0aef90b47c968e
Dennis Chen <atticcat@gmail.com>
2023-12-27 11:02:09 +0800

    refactor app entry and name

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,14 +1,14 @@
+import Expo from 'expo'
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
 import { Button, Text, View } from 'react-native'
-import Info from './screens/Info'
-import List from './screens/List'
-import Scroll from './screens/Scroll'
-import { appStyles, styles } from './styles'
+import Info from '@/screens/Info'
+import List from '@/screens/List'
+import Scroll from '@/screens/Scroll'
+import { appStyles, styles } from '@/styles'
 
 
-
-const pages = new Map([
+const screens = new Map([
     { name: 'info', title: 'Info', view: () => { return <Info /> } },
     { name: 'scroll', title: 'scroll', view: () => { return <Scroll /> } },
     { name: 'list', title: 'List', view: () => { return <List /> } }
@@ -16,16 +16,16 @@ const pages = new Map([
 
 export default function App() {
 
-    const [view, setView] = useState(pages.keys().next().value as string)
+    const [screen, setScreen] = useState(screens.keys().next().value as string)
 
     return (
         <View style={appStyles.app}>
-            <Text>View : {pages.get(view)?.title}</Text>
+            <Text>Screen : {screens.get(screen)?.title}</Text>
             <StatusBar style='light' translucent={false} />
             <View style={[styles.hlayout, { padding: 4, gap: 4, flexWrap: 'wrap' }]}>
-                {new Array(...pages.values()).map((e) => <Button key={e.name} title={e.title} onPress={() => { setView(e.name) }} />)}
+                {new Array(...screens.values()).map((e) => <Button key={e.name} title={e.title} onPress={() => { setScreen(e.name) }} />)}
             </View>
-            {pages.get(view)?.view()}
+            {screens.get(screen)?.view()}
         </View>
     )
 }
